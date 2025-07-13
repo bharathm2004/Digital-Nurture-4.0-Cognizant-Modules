22ba6214fc0d7faf8a2e99cf6d41e92db7f7e04d
bharathm2004 <125333644+bharathm2004@users.noreply.github.com>
2025-07-13 18:37:15 +0530

    Add files via upload

---
+++ Week 4/Spring Core – Load Country from Spring Configuration XML/spring-learn/src/main/java/service/HelloService.java
@@ -0,0 +1,13 @@
+package service;
+
+public class HelloService {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(message);
+    }
+}
